5c80d87abd285f3085822a361cd322424f4270b9
Bedford <qmal789@dgu.ac.kr>
2023-01-22 14:59:16 +0900

    camera Follow

---
+++ Assets/0.MyAssets/Scripts/PlayerCameraController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraController : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] float smoothing = 0.2f;
+    [SerializeField] Vector2 minCameraBoundary;
+    [SerializeField] Vector2 maxCameraBoundary;
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+    }
+    private void FixedUpdate()
+    {
+        Vector3 targetPos = new Vector3(player.position.x, player.position.y, this.transform.position.z);
+
+        targetPos.x = Mathf.Clamp(targetPos.x, minCameraBoundary.x, maxCameraBoundary.x);
+        targetPos.y = Mathf.Clamp(targetPos.y, minCameraBoundary.y, maxCameraBoundary.y);
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
+    }
+   
+}
